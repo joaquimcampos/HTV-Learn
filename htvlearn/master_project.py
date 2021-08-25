49939a0a5edbbccd4d7ce33c2d08ed633f9889a0
Joaquim Campos <joaquimcampos15@hotmail.com>
2021-08-25 16:48:36 +0200

    feat(master_project): add docstrings. add load_ckpt bool property.
    restore_model_data->restore_data.
    Remove unecessary things:
    remove get_best_model(). remove git stuff.

--- htvlearn/master_project.py
+++ htvlearn/master_project.py
@@ -1,8 +1,6 @@
 import os
 import collections
-import sys
 import glob
-import git
 import torch
 import numpy as np
 from abc import ABC, abstractproperty, abstractmethod
@@ -17,8 +15,14 @@ class MasterProject(ABC):
     # class attribute
     results_json_filename = 'results.json'
 
-    def __init__(self, params, write=True):
-        """ """
+    def __init__(self, params, log=True):
+        """
+        Args:
+            params (dict):
+                parameter dictionary.
+            log (bool):
+                if True, log results.
+        """
         self.params = params
         if self.params['verbose']:
             print('\n==> Parameters info: ', self.params, sep='\n')
@@ -28,26 +32,27 @@ class MasterProject(ABC):
 
         self.init_data()
         self.init_device()
-        if write is True:
+        if log is True:
             self.init_log()
             self.init_json()
 
-    def init_device(self):
-        """ """
-        if self.params['method'] != 'neural_net' or \
-                self.params['device'] == 'cpu':
-            self.device = 'cpu'
-            print('\nUsing CPU.')
+    def init_data(self):
+        """ Initialize data """
+        self.params['data']['log_dir'] = self.log_dir_model
+        self.data = Data(**self.params['data'])
 
-        elif self.params['device'] == 'cuda:0':
+    def init_device(self):
+        """Initialize running device (cpu or gpu)."""
+        if self.params['device'].startswith('cuda'):
             if torch.cuda.is_available():
-                self.device = 'cuda:0'
+                self.device = 'cuda:0'  # Using GPU0 by default
                 print('\nUsing GPU.')
             else:
                 self.device = 'cpu'
                 print('\nCUDA not available. Using CPU.')
         else:
-            raise ValueError(f'Cannot use device {self.params["device"]}.')
+            self.device = 'cpu'
+            print('\nUsing CPU.')
 
     def init_log(self):
         """ Create Log directory for training the model as :
@@ -56,46 +61,23 @@ class MasterProject(ABC):
         if not os.path.isdir(self.log_dir_model):
             os.makedirs(self.log_dir_model)
 
-        repo = git.Repo(search_parent_directories=True)
-        commit_sha = repo.head.object.hexsha
-
-        cmd_args = ' '.join(sys.argv)
-
-        # save commit sha and cmd line arguments corresponding to this model
-        save_str = '\n'.join(['Git commit sha: ', str(commit_sha)])
-        save_str += '\n\n'
-        save_str += '\n'.join(['Cmd args : ', cmd_args])
-
-        cmd_args_git_commit_filename = os.path.join(self.log_dir_model,
-                                                    'cmd_args_git_commit.txt')
-
-        with open(cmd_args_git_commit_filename, 'w') as text_file:
-            print(save_str, file=text_file)
-
-    def init_data(self):
-        """ Initialize data """
-        self.params['data']['log_dir'] = self.log_dir_model
-        self.data = Data(**self.params['data'])
-
     @abstractproperty
     def htv_log(self):
-        """ Should return htv log """
+        """ """
         pass
 
     @property
     def info_list(self):
-        """ Should return list of info to log on json file """
+        """List with info to log."""
         return ['train_mse', 'valid_mse', 'test_mse', 'htv']
 
     @property
     def sorting_key(self):
-        """ Key for sorting models in json file.
-        """
+        """Key for sorting models in json file."""
         return 'test_mse'
 
     def init_json(self):
-        """ Init json file for train/test results.
-        """
+        """Init json file for train/test results."""
         # initialize/verify json log file
         self.results_json = os.path.join(self.params['log_dir'],
                                          self.results_json_filename)
@@ -116,10 +98,15 @@ class MasterProject(ABC):
         json_dump(results_dict, self.results_json)
 
     def update_json(self, info, value):
-        """ Update json file with results
+        """
+        Update json file with latest/best validation/test accuracy/loss,
+        if training, and with test accuracy otherwise.
 
         Args:
-            info: e.g. 'df_loss'
+            info (str):
+                e.g. 'latest_valid_loss', 'best_train_acc'.
+            value (float):
+                value for the given info.
         """
         assert info in self.info_list, \
             f'{info} should be in {self.info_list}...'
@@ -150,7 +137,14 @@ class MasterProject(ABC):
 
     @abstractmethod
     def save_to_ckpt(self, ckpt_filename, save_dict=None):
-        """ Save params, data and htv to checkpoint.
+        """
+        Save params, data and htv to checkpoint.
+
+        Args:
+            ckpt_filename (str):
+                name of checkpoint where to log results.
+            save_dict (dict):
+                dictionary with additional info to log.
         """
         save_base_dict = {
             'params': self.params,
@@ -172,17 +166,17 @@ class MasterProject(ABC):
         torch.save(save_dict, ckpt_filename)
 
     def restore_ckpt_params(self):
-        """ Restore the parameters from a previously saved checkpoint
-        (either provided via --ckpt_filename or saved in log_dir/model_name)
-
-        Returns:
-            True if a checkpoint was successfully loaded and False otherwise.
         """
-        ckpt_None = 'ckpt_filename' not in self.params or \
-            self.params['ckpt_filename'] is None
+        Attempts to restore a checkpoint if resuming training or testing
+        a model.
 
-        ckpt_filename = None
-        if not ckpt_None:
+        If successful, it gets the loaded checkpoint and merges the saved
+        parameters.
+
+        Returns True if a checkpoint was successfully loaded,
+        and False otherwise.
+        """
+        if self.params['ckpt_filename'] is not None:
             ckpt_filename = self.params['ckpt_filename']
         elif self.params['restore']:
             ckpt_filename = \
@@ -204,18 +198,85 @@ class MasterProject(ABC):
             print('\nStarting from scratch.')
             return False
 
-    def restore_model_data(self):
-        """ """
-        # Load checkpoint.
-        print('\n==> Restoring from checkpoint...')
-        self.data = Data(data_from_ckpt=self.loaded_ckpt['data'],
-                         **self.params['data'])
+    @property
+    def load_ckpt(self):
+        """
+        Returns True if loading a checkpoint and restoring its parameters,
+        for resuming training or testing a model. Otherwise, returns False.
+        """
+        if ('ckpt_filename' in self.params and self.params["ckpt_filename"]
+                is not None) or (self.params["resume"] is True):
+            return True
+        else:
+            return False
 
-        return
+    @classmethod
+    def get_ckpt_from_log_dir_model(cls, log_dir_model):
+        """Get last checkpoint from log_dir_model (log_dir/model_name).
+
+        Args:
+            log_dir_model (str):
+                folder of the form log_dir/model_name
+        """
+        files = cls.get_all_ckpt_from_log_dir_model(log_dir_model)
+
+        if len(files) > 0:
+            ckpt_filename = files[-1]
+            print(f'Restoring {ckpt_filename}')
+            return ckpt_filename
+        else:
+            print(f'No ckpt found in {log_dir_model}...')
+            return None
+
+    @staticmethod
+    def get_all_ckpt_from_log_dir_model(log_dir_model):
+        """
+        Get all checkpoints from log_dir_model (log_dir/model_name).
+
+        Args:
+            log_dir_model (str):
+                folder of the form log_dir/model_name
+        """
+        regexp_ckpt = os.path.join(log_dir_model, '*.pth')
+
+        files = glob.glob(regexp_ckpt)
+        files.sort(key=os.path.getmtime)  # sort by time from oldest to newest
+
+        return files
+
+    @classmethod
+    def load_ckpt_params(cls, ckpt_filename, flatten=True, map_loc=None):
+        """
+        Returns the ckpt dictionary and the parameters saved
+        in a checkpoint file.
+
+        Args:
+            ckpt_filename (str):
+                Name of checkpoint (.pth) file.
+            flatten (bool):
+                whether to flatten the structure of the parameters dictionary
+                into a single level
+                (see structure in struct_default_values.py).
+        """
+        ckpt = cls.get_loaded_ckpt(ckpt_filename, map_loc=map_loc)
+        params = ckpt['params']
+
+        if flatten is True:
+            params = flatten_structure(params)
+
+        return ckpt, params
 
     @staticmethod
     def get_loaded_ckpt(ckpt_filename, map_loc=None):
-        """ Returns a loaded checkpoint from ckpt_filename, if it exists.
+        """
+        Returns a loaded checkpoint (ckpt dictionary)
+        from ckpt_filename, if it exists.
+
+        Args:
+            ckpt_filename (str):
+                Name of checkpoint (.pth) file.
+            map_loc (str):
+                for mapping device location.
         """
         assert map_loc is None or \
             map_loc.startswith('cuda') or map_loc.startswith('cpu')
@@ -234,51 +295,16 @@ class MasterProject(ABC):
         return ckpt
 
     @classmethod
-    def load_ckpt_params(cls, ckpt_filename, flatten=True, map_loc=None):
-        """ Returns the parameters saved in a checkpoint.
+    def load_results_dict(cls, log_dir):
+        """
+        Load results from the results json file in log_dir.
 
         Args:
-            flatten: whether to flatten the structure of the
-                     parameters dictionary
-            into a single level (see structure in struct_default_values.py)
-            map_loc: map_location for torch.load.
-        """
-        ckpt = cls.get_loaded_ckpt(ckpt_filename, map_loc=map_loc)
-        params = ckpt['params']
+            log_dir (str):
+                log directory where results json file is located.
 
-        if flatten is True:
-            params = flatten_structure(params)
-
-        return ckpt, params
-
-    @classmethod
-    def get_ckpt_from_log_dir_model(cls, log_dir_model):
-        """ Get last ckpt from log_dir_model (log_dir/model_name)
-        """
-        files = cls.get_all_ckpt_from_log_dir_model(log_dir_model)
-
-        if len(files) > 0:
-            ckpt_filename = files[-1]
-            print(f'Restoring {ckpt_filename}')
-            return ckpt_filename
-        else:
-            print(f'No ckpt found in {log_dir_model}...')
-            return None
-
-    @staticmethod
-    def get_all_ckpt_from_log_dir_model(log_dir_model):
-        """ Get last ckpt from log_dir_model (log_dir/model_name)
-        """
-        regexp_ckpt = os.path.join(log_dir_model, '*.pth')
-
-        files = glob.glob(regexp_ckpt)
-        files.sort(key=os.path.getmtime)  # sort by time from oldest to newest
-
-        return files
-
-    @classmethod
-    def load_results_dict(cls, log_dir):
-        """ Load results dictionary from results json file in log_dir.
+        Returns:
+            results_dict (dict): dictionary with logged results.
         """
         results_json = os.path.join(log_dir, cls.results_json_filename)
         results_dict = json_load(results_json)
@@ -287,22 +313,23 @@ class MasterProject(ABC):
 
     @classmethod
     def dump_results_dict(cls, results_dict, log_dir):
-        """ Dump results dictionary in results json file in log_dir.
+        """
+        Dump results dictionary in the results json file in log_dir.
+
+        Args:
+            results_dict (dict):
+                dictionary with logged results.
+            log_dir (str):
+                log directory where results json file is located.
         """
         results_json = os.path.join(log_dir, cls.results_json_filename)
         json_dump(results_dict, results_json)
 
-    @classmethod
-    def get_best_model(cls, log_dir, mode='train'):
-        """
-        Get the name and checkpoint of the best model (best validation/test)
-        from the train/test results (saved in log_dir/[mode]_results.json).
-        """
-        results_dict = cls.load_results_dict(log_dir, mode)
+    def restore_data(self):
+        """Restore data from loaded checkpoint."""
+        # Load checkpoint.
+        print('\n==> Restoring from checkpoint...')
+        self.data = Data(data_from_ckpt=self.loaded_ckpt['data'],
+                         **self.params['data'])
 
-        # models are ordered by validation accuracy; choose first one.
-        best_model_name = next(iter(results_dict))
-        log_dir_best_model = os.path.join(log_dir, best_model_name)
-        ckpt_filename = cls.get_ckpt_from_log_dir_model(log_dir_best_model)
-
-        return best_model_name, ckpt_filename
+        return
