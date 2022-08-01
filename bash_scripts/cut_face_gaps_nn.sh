228a6f8194d9d28cbb51af0aefcb9a25695ca15b
joaquim.campos <joaquimcampos15@hotmail.com>
2022-08-01 10:58:12 +0100

    feat(scripts): Add weight decay to model name for nn

--- bash_scripts/cut_face_gaps_nn.sh
+++ bash_scripts/cut_face_gaps_nn.sh
@@ -19,7 +19,7 @@ do
     taskset --cpu-list "$cpu" python3 "$REPO"/htvlearn/main.py \
     --method neural_net --num_hidden_neurons "$hidden" \
     --log_dir "$REPO"/output/nn/nn_"$nb"_wd_"$wd" \
-    --model_name nn_cut_face_gaps_seed_"$seed"_num_train_"$nb"_hidden_"$hidden" \
+    --model_name nn_cut_face_gaps_seed_"$seed"_num_train_"$nb"_hidden_"$hidden"_wd_"$wd" \
     --dataset_name cut_face_gaps --num_train "$nb" \
      --noise_ratio 0 --seed "$seed" -v
 done
